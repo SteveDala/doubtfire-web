12efa7ffab18e38f75fceb639e559d7034bce941
samindiii <samindi200222@gmail.com>
2025-09-14 17:55:22 +1000

    chore: declare new summary component in module

--- src/app/doubtfire-angular.module.ts
+++ src/app/doubtfire-angular.module.ts
@@ -292,7 +292,7 @@ import {PrivacyPolicy} from './config/privacy-policy/privacy-policy';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import { StaffGrantExtensionComponent } from './units/states/tasks/staff-grant-extension/staff-grant-extension.component';
 import { StaffGrantExtensionFormComponent } from './units/states/tasks/staff-grant-extension/grant-extension-form/grant-extension-form.component';
-
+import { StaffGrantExtensionSummaryComponent } from './units/states/tasks/staff-grant-extension/staff-grant-extension-summary/staff-grant-extension-summary.component';
 @NgModule({
   // Components we declare
   declarations: [
@@ -454,6 +454,7 @@ import { StaffGrantExtensionFormComponent } from './units/states/tasks/staff-gra
     TaskVisualisationComponent
     NotificationsButtonComponent,
     StaffGrantExtensionComponent,
+    StaffGrantExtensionSummaryComponent,
     StaffTaskListComponent,
   ],
   providers: [
